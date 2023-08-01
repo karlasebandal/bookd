2c690fda13c9da9c068db4e1337daab5b39897f2
Karla Degrano-Sebandal <karla.degrano@gmail.com>
2023-08-01 08:39:04 +0800

    handleChange function added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import React, { useState } from "react";
 import banner from '../assets/images/raft-photos/banner.jpg';
 
 const Banner = () => {
+
+  const [value, setValue] = useState()
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
   return (
 
     <div class="flex flex-wrap items-center justify-between">
@@ -14,14 +22,16 @@ const Banner = () => {
           
           <div class="items-center justify-between">
               <label for="services" class="block mb-2 text-sm font-medium text-rescue-orange dark:text-white rounded-lg">Pick your adventure</label>
-                <select id="services" class="bg-gray-50 border border-navy-blue text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option>Whitewater Rafting</option>
-                    <option>Rope Access Services</option>
-                    <option>Team Building</option>
-                    <option>Manufacturing</option>
-                    <option>Water Sports Training</option>
+                <select value={value} onChange={handleChange} id="services" class="bg-gray-50 border border-navy-blue text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="rafting">Whitewater Rafting</option>
+                    <option value="rope">Rope Access Services</option>
+                    <option value="team building">Team Buizlding</option>
+                    <option value="manufacturing">Manufacturing</option>
+                    <option value="training">Water Sports Training</option>
                 </select>
           </div>
+
+          <p>I chose {value}</p>
         </div>
       </div>
     </div>
